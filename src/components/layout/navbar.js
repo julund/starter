a8b87db9d9c5da2aa808882aa0a17634032192be
julund <julu900@gmail.com>
2021-01-28 14:33:46 +0000

    components/l:

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -23,18 +23,19 @@ import { useState } from "react"
 export default function Navbar() {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-      <nav className="sticky top-0 flex flex-wrap items-center justify-between p-4 gray-200 font-title">
+      <nav className="sticky top-0 flex flex-wrap items-center justify-between p-4 bg-gray-200">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between gap-2">
-          <div className="w-full relative flex justify-between items-center md:w-auto md:static md:block md:justify-start mr-2 md:mr-6">
-            <Link className="inline-block whitespace-no-wrap" href="/" type="nav">Starter</Link>
+          {/* <div className="w-full relative flex justify-between items-center md:w-auto md:static md:block md:justify-start mr-2 md:mr-6 font-title"> */}
+          <div className="w-full flex justify-between items-center md:w-auto md:justify-start mr-2 md:mr-6 font-title">
+            <Link className="items-center border-b-4 whitespace-no-wrap" href="/">Starter</Link>
             <a role="button" className="block md:hidden select-none" aria-label="menu" aria-expanded={navbarOpen} onClick={() => setNavbarOpen(!navbarOpen)}>
                  {navbarOpen ? <XSolid className="h-6 fill-current"/> : <MenuSolid className="h-6 fill-current"/>}
             </a>
           </div>
           <div className={"md:flex flex-col md:flex-row flex-grow items-center gap-2 md:gap-6" + (navbarOpen ? " flex" : " hidden")}>
-              <Link className="items-center" href="/services" type="nav">Services</Link>
-              <Link className="items-center" href="/about-us" type="nav">About us</Link>
-              <Link className="items-center" href="/contact" type="nav">Contact</Link>
+              <Link className="items-center border-b-4 hover:border-primary-300" activeClassName="border-primary-300" inactiveClassName="border-transparent" href="/services">Services</Link>
+              <Link className="items-center border-b-4 hover:border-primary-300" activeClassName="border-primary-300" inactiveClassName="border-transparent" href="/about-us">About us</Link>
+              <Link className="items-center border-b-4 hover:border-primary-300" activeClassName="border-primary-300" inactiveClassName="border-transparent" href="/contact">Contact</Link>
           </div>
         </div>
       </nav>
--- src/components/link.js
+++ src/components/link.js
@@ -1,19 +1,13 @@
 import { useRouter } from 'next/router'
 import NextLink from 'next/link'
 
-const Link = (props) => {
-    if (!props.href) return null
+const Link = ({href, className = '', activeClassName = '', inactiveClassName = '', children, ...rest}) => {
+    if (!href) return null
     const router = useRouter()
-    const isActive = (router.pathname === props.href)
-    const styles = {
-        'normal': `hover:underline ${isActive ? 'text-primary-700' : 'text-primary-800'}`,
-        'button': `px-4 py-2 rounded-md hover:bg-gray-50 hover:text-primary-600 ${isActive ? 'bg-gray-100 text-primary-700' : 'bg-gray-200 text-primary-800'}`,
-        'nav': `border-b-4 hover:border-primary-300 ${isActive ? 'border-primary-300' : 'border-transparent'}`
-   }
-   const style = `${styles[props.type || 'normal']}${props.className ? ' ' + props.className : ''}`
+    const allClasses = `${className} ${(router.pathname === href) ? activeClassName : inactiveClassName}`
     return (
-        <NextLink href={props.href}>
-            <a {...props} className={style}>{props.children}</a>
+        <NextLink href={href}>
+            <a {...rest} className={allClasses}>{children}</a>
         </NextLink>
     )
 }
