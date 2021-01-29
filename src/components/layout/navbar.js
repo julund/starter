9179bd6fc7330fb80c37b819eba1664cd5c98f35
julund <julu900@gmail.com>
2021-01-29 13:46:28 +0000

    Acot

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -1,30 +1,41 @@
 import Link from "../link";
 import { MenuSolid, XSolid } from '@graywolfai/react-heroicons'
 import { useState } from "react"
+import {useSpring, animated, config} from 'react-spring'
+// import { Keyframes, animated } from 'react-spring/renderprops.cjs'
+
+const Menu = ({ children, props, state }) =>
+    <animated.div
+        className={"md:flex flex-col md:flex-row flex-grow items-center gap-2 md:gap-6" + (state ? " flex" : " hidden")}
+        style={props}
+    >{children}</animated.div>
+
+const MenuItem = ({children, href}) => <Link onClick={() => setIsOpen(false)} className="items-center border-b-4 hover:border-primary-300" activeClassName="border-primary-300" inactiveClassName="border-transparent" href={href}>{children}</Link>
 
 export default function Navbar() {
 
     const [isOpen, setIsOpen] = useState(false);
+    
+    const props = useSpring({
+        opacity: isOpen ? 1 : 0,
+        height: isOpen ? 115 : 0,
+        config: config.default,
+    })
 
     return (
         <nav className="sticky top-0 flex flex-wrap items-center justify-between p-4 bg-gray-200">
-            <div className="container px-4 mx-auto flex flex-wrap items-center justify-between gap-2">
+            <div className="container md:px-4 mx-auto flex flex-wrap items-center justify-between gap-2">
                 <div className="w-full flex justify-between items-center md:w-auto md:justify-start mr-2 md:mr-6 font-title">
                     <Link className="items-center border-b-4 whitespace-no-wrap" href="/">Starter</Link>
                     <a role="button" className="block md:hidden select-none" aria-label="menu" aria-expanded={isOpen} onClick={() => setIsOpen(!isOpen)}>
                         {isOpen ? <XSolid className="h-6 fill-current" /> : <MenuSolid className="h-6 fill-current" />}
                     </a>
                 </div>
-                <div className={"md:flex flex-col md:flex-row flex-grow items-center gap-2 md:gap-6" + (isOpen ? " flex" : " hidden")}>
-                    <Link className="items-center border-b-4 hover:border-primary-300" activeClassName="border-primary-300" inactiveClassName="border-transparent" href="/services">Services</Link>
-                    <Link className="items-center border-b-4 hover:border-primary-300" activeClassName="border-primary-300" inactiveClassName="border-transparent" href="/about-us">About us</Link>
-                    <Link className="items-center border-b-4 hover:border-primary-300" activeClassName="border-primary-300" inactiveClassName="border-transparent" href="/contact">Contact</Link>
-                </div>
-                {/* <div className={"md:flex flex-col md:flex-row flex-grow items-center gap-2 md:gap-6" + (isOpen ? " flex" : " hidden")}>
-              <Link className="items-center border-b-4 hover:border-primary-300" activeClassName="border-primary-300" inactiveClassName="border-transparent" href="/services">Services</Link>
-              <Link className="items-center border-b-4 hover:border-primary-300" activeClassName="border-primary-300" inactiveClassName="border-transparent" href="/about-us">About us</Link>
-              <Link className="items-center border-b-4 hover:border-primary-300" activeClassName="border-primary-300" inactiveClassName="border-transparent" href="/contact">Contact</Link>
-          </div> */}
+                <Menu props={props} state={isOpen}>
+                        <MenuItem href="/services">Services</MenuItem>
+                        <MenuItem href="/about-us">About us</MenuItem>
+                        <MenuItem href="/contact">Contact</MenuItem>
+                </Menu>
             </div>
         </nav>
     );
